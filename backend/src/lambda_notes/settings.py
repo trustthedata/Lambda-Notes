ceea6790c469e9b88d7bd7e5faba6d9db122bfb3
Herbert Hodgson <herberthodgson@hotmail.com>
2018-10-31 20:23:07 -0400

    Invalid syntax in settings.py line 13 import fixed.

--- backend/src/lambda_notes/settings.py
+++ backend/src/lambda_notes/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import dj-database-url
+import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
