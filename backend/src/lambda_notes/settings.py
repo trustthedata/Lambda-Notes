ca5cc6af16cb1958cb24d5fa5f6473d6113c7693
Herbert Hodgson <herberthodgson@hotmail.com>
2018-10-31 20:31:51 -0400

    Attempting to fix a NameError with DATABASES not being defined

--- backend/src/lambda_notes/settings.py
+++ backend/src/lambda_notes/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 import os
 from datetime import timedelta
